ffba769c76a5acf617d58296926240eef99c03c1
Narwar04 <carenathan027@gmail.com>
2025-04-13 06:26:14 -0400

    started Hannah Hales crazy idea

---
+++ Appanator/Appinator.js
@@ -0,0 +1,84 @@
+const professorProfiles = 
+[
+  { 
+    name: "Pierre Cagne",
+    answers: ["yes", "yes", "yes", "yes", "yes"]
+  },
+  { 
+    name: "Andrew Polonsky",
+    answers: ["yes", "yes", "yes", "yes", "no"]
+  },
+  { 
+    name: "James Wilkes",
+    answers: ["yes", "yes", "yes", "no", "yes"]
+  },
+  { 
+    name: "Mitch Parry",
+    answers: ["yes", "yes", "yes", "no", "no", "yes"]
+  },
+  { 
+    name: "Mark Hills",
+    answers:["yes", "yes", "yes", "no", "no", "no"]
+  },
+  { 
+    name: "Rahman Tashakkori",
+    answers:["yes", "yes", "no", "yes", "yes"]
+  },
+  { 
+    name: "Jay Fenwick",
+    answers:["yes", "yes", "no", "yes", "no"]
+  },
+  { 
+    name: "Tinghao Feng",
+    answers:["yes", "yes", "no", "no", "yes", "yes", "yes"]
+  },
+  { 
+    name: "Jonathan Rink",
+    answers:["yes", "yes", "no", "no", "yes", "yes", "no"]
+  },
+  { 
+    name: "Mohammad Ali Javidian",
+    answers:["yes", "yes", "no", "no", "yes", "no", "yes"]
+  },
+  { 
+    name: "Baset Hamza",
+    answers:["yes", "yes", "no", "no", "yes", "no", "no"]
+  },
+  { 
+    name: "Joel Swanson",
+    answers:["yes", "no", "yes"]
+  },
+  { 
+    name: "Val Lapensée-Rankineal",
+    answers:["no", "yes", "yes", "yes"]
+  },
+  { 
+    name: "Danielle  Lapensée-Rankineal",
+    answers:["no", "yes", "yes", "no"]
+  },
+  { 
+    name: "Patricia Johann",
+    answers:["no", "yes", "no",]
+  },
+  { 
+    name: "Michelle Melton",
+    answers:["no", "no", "yes", "yes"]
+  },
+  { 
+    name: "Cindy Norris",
+    answers:["no", "no", "yes", "no"]
+  },
+  { 
+    name: "Cortney Dixon",
+    answers:["no", "no", "no", "yes", "yes"]
+  },
+  { 
+    name: "Yeganeh Madadi",
+    answers:["no", "no", "no", "yes", "no", "yes"]
+  },
+  { 
+    name: "Nazia Sharmin",
+    answers:["no", "no", "no", "yes", "no", "no"]
+  },
+
+];
