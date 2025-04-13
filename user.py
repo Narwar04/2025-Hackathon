40ec5d903db8a146f1b0cf564fda3c1c1aebbf5e
blaezj <blaezjib@icloud.com>
2025-04-12 23:04:25 -0400

    generating chat messages

---
+++ user.py
@@ -0,0 +1,43 @@
+import random
+from server import ryansServer
+
+messages_default = ["POG", "LMAO", "BAD", "L", "-2"] 
+
+class User:
+    id: int               # id for user
+    name: str             # name of user
+    lmmessages: list[str] # list of generated lm messages
+    messages: list[str]   # default messages
+
+    def __init__(self, id, name, lmmessages): #initialization func
+        self.id = id
+        self.name = name
+        self.lmmessages = lmmessages
+        self.messages = messages_default
+
+    def addMessage(self, message: str):
+        self.messages.append(message)
+
+    def sendDefaultMessage(self):
+        msglen = len(self.messages) - 1
+        msg = self.name + ": " + self.messages[random.randint(0, msglen)]
+        return msg
+    
+    def sendLMMessage(self, prompt:str, server:ryansServer):
+        server.sendPrompt(prompt)
+        msg = server.recieveResonce()
+        msg = self.name + ": " + msg
+        return msg
+
+
+    
+
+
+
+# user1 = User(21, "user21", [], [])
+
+# # print(user1.messages)
+# user1.addMessage("Hello!!!!")
+# # print(user1.messages)
+# user1.displayMessage(message=(user1.messages[0]))
+
